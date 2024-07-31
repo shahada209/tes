637b20923ac33d6702c3b08dad1af30299635310
shahada209 <asyifa_shahada_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:55:39 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,10 @@
+public class tes {
+
+   public static void main(String[] args) {
+System.out.println("Nama: Asyifa Almira Shahada");
+System.out.println("Alamat: Desa Rejosari, kecamatan wonodadi, kabupaten Blitar");
+System.out.println("SMP: Mtsn 1 Blitar");
+System.out.println("Nomer HP: 085792450396");
+
+}
+}
